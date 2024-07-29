b01f495b5d37159a2eeed2268babfc13b79ef81f
noah873 <157926076+noah873@users.noreply.github.com>
2024-07-28 17:37:00 -0700

    Add files via upload

---
+++ driver.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+from crawler import * # Frontier class and crawlerThread() function imported
+
+def connectDatabase():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['search_engine']
+    return db
+
+def main():
+    seedURL = "https://www.cpp.edu/cba/international-business-marketing/index.shtml"
+    frontier = Frontier(seedURL)
+
+    num_targets = 10
+
+    crawlerThread(frontier, num_targets)
+
+if __name__ == "__main__":
+    main()
