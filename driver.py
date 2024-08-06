b2e8f2ab0738c394cfdb7c11b9784b7c950b70fd
noah873 <157926076+noah873@users.noreply.github.com>
2024-08-06 16:26:59 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,7 +4,7 @@ from text_transformation import transformPages  # Import text transformation fun
 def main():
     seedURL = "https://www.cpp.edu/cba/international-business-marketing/index.shtml"
     frontier = Frontier(seedURL) # Frontier class acts like a queue, initialized with just the seed url
-    num_targets = 10 # crawler will run until it has found this many target pages
+    num_targets = 22 # crawler will run until it has found this many target pages
 
     crawlerThread(frontier, num_targets) # crawls webpages from seed URL and saves all pages in db.crawled_pages until num_targets of target pages are found
 
