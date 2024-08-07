f7eb6ccbc3f2734157a6d1d82b785c2af9a7aa7b
noah873 <157926076+noah873@users.noreply.github.com>
2024-08-06 18:13:52 -0700

    Update index_creation.py

--- index_creation.py
+++ index_creation.py
@@ -1,6 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from db_connection import connectDatabase
-from text_transformation import text_transformation
 
 # Connect to the database
 db = connectDatabase()
